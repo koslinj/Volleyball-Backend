94bc63426ddb5a8f9f7f90b4d1b830c3a425f576
koslinj <koslinj@gmail.com>
2024-05-25 15:04:05 +0200

    creating new team

--- functions/teams.js
+++ functions/teams.js
@@ -10,6 +10,20 @@ const fetchAllTeams = async () => {
   }
 };
 
+const createTeam = async (body) => {
+  try {
+    const playersJSON = { players: body.players }
+    const res = await client.query(`
+      INSERT INTO teams (name, players) VALUES ($1, $2)`,
+      [body.name, playersJSON]);
+    return true;
+  } catch (error) {
+    console.error('Error fetching matches:', error);
+    return false;
+  }
+};
+
 module.exports = {
   fetchAllTeams,
+  createTeam
 }
--- routes/referee.js
+++ routes/referee.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { fetchAllMatches, deleteMatch } = require('../functions/matches');
-const { fetchAllTeams } = require('../functions/teams');
+const { fetchAllTeams, createTeam } = require('../functions/teams');
 const { verifyToken } = require('../middleware/authMiddleware')
 
 // Protected route
@@ -30,6 +30,17 @@ router.get('/teams/all', verifyToken, async (req, res) => {
   if (req.userRole === "referee") {
     const rows = await fetchAllTeams();
     res.send(rows)
+  } else {
+    res.status(403).json({ message: 'Forbidden' });
+  }
+});
+
+// Protected route
+router.post('/teams', verifyToken, async (req, res) => {
+  if (req.userRole === "referee") {
+    const success = await createTeam(req.body);
+    if (!success) res.status(404).json({ message: 'There was a problem while creating!' });
+    res.status(200).json({ message: 'Created successfully!' });
   } else {
     res.status(403).json({ message: 'Forbidden' });
   }
