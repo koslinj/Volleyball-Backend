e06b1b105fc0020d6e24f9ac35bd37ef1951cbd5
koslinj <koslinj@gmail.com>
2024-05-29 17:38:30 +0200

    remove check for role in observator endpoints

--- routes/observator.js
+++ routes/observator.js
@@ -5,32 +5,20 @@ const { verifyTokenMiddleware } = require('../middleware/authMiddleware')
 
 // Protected route
 router.get('/matches/all', verifyTokenMiddleware, async (req, res) => {
-  if (req.userRole === "observator") {
-    const rows = await fetchAllMatches();
-    res.send(rows)
-  } else {
-    res.status(403).json({ message: 'Forbidden' });
-  }
+  const rows = await fetchAllMatches();
+  res.send(rows)
 });
 
 // Protected route
 router.get('/matches/all/:status', verifyTokenMiddleware, async (req, res) => {
-  if (req.userRole === "observator") {
-    const rows = await fetchMatchesByStatus(req.params.status);
-    res.send(rows)
-  } else {
-    res.status(403).json({ message: 'Forbidden' });
-  }
+  const rows = await fetchMatchesByStatus(req.params.status);
+  res.send(rows)
 });
 
 // Protected route
 router.get('/matches/:id', verifyTokenMiddleware, async (req, res) => {
-  if (req.userRole === "observator") {
-    const rows = await fetchMatchDetailsById(req.params.id);
-    res.send(rows)
-  } else {
-    res.status(403).json({ message: 'Forbidden' });
-  }
+  const rows = await fetchMatchDetailsById(req.params.id);
+  res.send(rows)
 });
 
 module.exports = router;
